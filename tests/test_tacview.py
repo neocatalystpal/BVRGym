0dc9be9b230a01b240cb0f1093bdd6061e647c81
xcwoid <edvards.scukins@gmail.com>
2026-01-29 10:40:07 +0100

    forgot trunk param

--- tests/test_tacview.py
+++ tests/test_tacview.py
@@ -7,7 +7,7 @@ obs = env.reset()
 done = False
 while not done:
     action = env.action_space.sample()  # Sample a random action
-    obs, reward, done, info = env.step(action)
+    obs, reward, done, trunk, info = env.step(action)
     env.log_tacview()
 print(info)
 print("View data in Tacview from data_output/tacview/")
